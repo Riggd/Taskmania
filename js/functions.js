24966e57993d944f6bbacc4f490f4724b23da38f
Derek Onay <wr3tched@gmail.com>
2014-05-20 16:55:21 +0200

    Functioning Delete, now to deal with the pesky add-new-task

--- js/functions.js
+++ js/functions.js
@@ -1,27 +1,23 @@
-add_task();
-show_tasks();
-show_archive();
-delete_task();
+$(document).ready(function() {
 
-function add_task() {
     $('.add-task').submit(function(){
-    var new_task = $('.add-task input[name=new-task]').val();
-    
-    // Change to task view and fadeout archive items
-    $('.archive-item').fadeOut();
-    
-    if(new_task != ''){
-        $.post('includes/add-task.php', { task: new_task }, function( data ) {
-            $('.add-task input[name=new-task]').val('');
-            $(data).appendTo('.todo-list ul').hide().fadeIn();
-        });
-    }
-    return false; // Ensure that the form does not submit twice
+        var new_task = $('.add-task input[name=new-task]').val();
+        
+        // Change to task view and fadeout archive items
+        $('.archive-item').fadeOut();
+        
+        if(new_task != ''){
+            $.post('includes/add-task.php', { task: new_task }, function( data ) {
+                $('.add-task input[name=new-task]').val('');
+                $(data).appendTo('.todo-list ul').hide().fadeIn();
+            });
+        }
+        return false; // Ensure that the form does not submit twice
     });
-}
 
-function delete_task() {
+
     $('.delete-button').click( function(){
+        $.get('includes/view-task.php'); 
         var current_element = $(this);
         var id = $(this).attr('id');
                
@@ -30,24 +26,22 @@ function delete_task() {
             alert('it worked');
         });
     });
-}
 
-function show_tasks() {
+
+
     $('.task-button').click(function(){
-        var current_element = $(this);
-        var id = $(this).attr('id');       
            
-        $.post('includes/view-task.php', { task: id }, function(task) {
+        $.post('includes/view-task.php' , function(task) {
             $(task).appendTo('.todo-list ul').hide().fadeIn(); 
         });
         
         $('.archive-item').fadeOut("slow", function() { $('.archive-item').remove(); });
         $('.task-button').attr('disabled','disabled');     
-        $('.archive-button').attr('disabled',false);        
+        $('.archive-button').attr('disabled',false);
     });
- }
 
-function show_archive() { 
+
+
     $('.archive-button').click(function(){
         var current_element = $(this);
         var id = $(this).attr('id');
@@ -60,4 +54,5 @@ function show_archive() {
         $('.archive-button').attr('disabled','disabled');     
         $('.task-button').attr('disabled',false);  
     });
-}
+
+});
